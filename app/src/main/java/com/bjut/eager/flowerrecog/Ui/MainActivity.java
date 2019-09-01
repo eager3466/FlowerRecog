a891c997bd5a10317c33c1194b0b647f498e44d5
--global <yuhuaqing@xiaomi.com>
2019-09-01 16:31:15 +0800

    change 2 by lsl
    Signed-off-by: --global <yuhuaqing@xiaomi.com>

--- app/src/main/java/com/bjut/eager/flowerrecog/Ui/MainActivity.java
+++ app/src/main/java/com/bjut/eager/flowerrecog/Ui/MainActivity.java
@@ -225,6 +225,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
             choosePhotoFromAlbum();
         }
 	// made change 1 from yhq. Base on no anthoer line
+
+        // made change 2 from lsl. Base on yhq's change1.
     }
 
 }
